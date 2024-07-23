a9910ec67c5847316bf32e2f5c353c962a636344
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-07-23 09:48:35 +0200

    Ajout fonction input_sentence

---
+++ analyse_frequence_lettres_texte.py
@@ -0,0 +1,12 @@
+"""
+Analyse de la fréquence des lettres d'un texte
+"""
+
+def input_sentence():
+    """
+    Demande à l’utilisateur l’entrée d’une phrase
+    :return: La phrase
+    """
+
+    user_input = input("Enter sentence :")
+    return user_input
