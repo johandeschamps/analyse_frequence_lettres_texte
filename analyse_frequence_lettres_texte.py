61eedf83521c953ff4e5c62982e4baacc9daf7f9
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-07-23 12:58:55 +0200

    Ajout fonction input_sentence

--- analyse_frequence_lettres_texte.py
+++ analyse_frequence_lettres_texte.py
@@ -1,6 +1,8 @@
 """
 Analyse de la fréquence des lettres d'un texte
 """
+import unicodedata
+
 
 def input_sentence():
     """
@@ -10,3 +12,15 @@ def input_sentence():
 
     user_input = input("Enter sentence :")
     return user_input
+
+
+def normalize_sentence(string):
+    """
+    Normalise une phrase en la convertissant en minuscules et en supprimant les accents.
+    :param input_str:Chaine de caractère
+    :return: La phrase en minuscule et sans accent
+    """
+    string = string.lower()
+    string = unicodedata.normalize('NFKD', string)
+    chaine = ''.join([c for c in string if not unicodedata.combining(c)])
+    return string
